1d08d9758334916b10c887e48cfbf629124745e7
AlexDoes <alexsw.191@gmail.com>
2023-09-06 22:51:55 -0400

    Connected to websocket

--- frontend/init.js
+++ frontend/init.js
@@ -1,23 +1,25 @@
 // Establish the WebSocket connection
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket("ws://localhost:8080/2364966");
 
-socket.onopen = function(e) {
+socket.onopen = function (e) {
   console.log("[open] Connection established to localhost:8080");
 };
 
-socket.onmessage = function(event) {
+socket.onmessage = function (event) {
   console.log(`[message] Data received from server: ${event.data}`);
 };
 
-socket.onerror = function(error) {
+socket.onerror = function (error) {
   console.log(`[error] ${error.message}`);
 };
 
-socket.onclose = function(event) {
+socket.onclose = function (event) {
   if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code}, reason=${event.reason}`);
+    console.log(
+      `[close] Connection closed cleanly, code=${event.code}, reason=${event.reason}`
+    );
   } else {
-    console.log('[close] Connection died');
+    console.log("[close] Connection died");
   }
 };
 
