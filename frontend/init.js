83e6093a7cdc3d2c7ac8f82c69cbb8fe4abdb63f
Peter Joh <johpeter@gmail.com>
2023-09-06 20:46:15 -0600

    alex test this

--- frontend/init.js
+++ frontend/init.js
@@ -1,63 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let flag8 = 0;
+// Establish the WebSocket connection
+const socket = new WebSocket('ws://localhost:8080');
 
-  var replaceButton = document.getElementById("replaceButton");
-  replaceButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (tabs && tabs.length > 0) {
-        var activeTab = tabs[0];
-        chrome.scripting.executeScript(
-          {
-            target: { tabId: activeTab.id },
-            func: contentScriptFunction,
-            args: [flag8],
-          },
-          (results) => {
-            if (results && results.length > 0) {
-              flag8 = results[0].result;
-            }
-          }
-        );
-      } else {
-        console.error("No active tab found.");
-      }
-    });
-  });
-});
+socket.onopen = function(e) {
+  console.log("[open] Connection established to localhost:8080");
+};
 
-function contentScriptFunction(flag8) {
-  var elements = document.getElementsByTagName("*");
-  let searchPattern = /Alex/gi;
+socket.onmessage = function(event) {
+  console.log(`[message] Data received from server: ${event.data}`);
+};
 
-  if (flag8 % 2 !== 0) {
-    // Replace 'Alex' with the image
-    let alexImage = document.getElementById("alexImage");
-    if (alexImage) {
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        if (element.hasChildNodes()) {
-          for (var j = 0; j < element.childNodes.length; j++) {
-            var node = element.childNodes[j];
-            if (node.nodeType === Node.TEXT_NODE) {
-              node.textContent = node.textContent.replace(searchPattern, "");
-              element.appendChild(alexImage.cloneNode(true));
-            }
-          }
-        }
-      }
-    } else {
-      console.error("Alex image element not found.");
-    }
+socket.onerror = function(error) {
+  console.log(`[error] ${error.message}`);
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    console.log(`[close] Connection closed cleanly, code=${event.code}, reason=${event.reason}`);
   } else {
-    // Replace the image with 'Alex'
-    let alexImages = document.querySelectorAll("#alexImage");
-    for (var i = 0; i < alexImages.length; i++) {
-      var image = alexImages[i];
-      image.parentNode.replaceChild(document.createTextNode("Alex"), image);
-    }
+    console.log('[close] Connection died');
   }
+};
 
-  // Return the toggled flag value.
-  return 1 - flag8;
-}
+// Add any other functionality you might want, e.g., sending data, processing incoming data, etc.
