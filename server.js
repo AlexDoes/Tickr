40dff125a97bd088a5269c0d98b7d9dfc958afff
AlexDoes <alexsw.191@gmail.com>
2023-09-06 22:08:49 -0400

    initial push

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ wss.on("connection", async function connection(ws, req) {
   ws.on("ping", () => {
     ws.pong();
   });
-  
+
   console.log(req.url);
 
   const jsfiles = await glob(`**/${req.url}_events.jsonl`, {
     ignore: "node_modules/**",
   });
 
-  console.log('Emitting', jsfiles[0]);
+  console.log("Emitting", jsfiles[0]);
   const eventsFile = jsonlFile(jsfiles[0]);
   eventsFile.read((line) => ws.send(JSON.stringify(line)));
 });
