c3ddb88270e825fa5feaf3d0706f90c064085b0c
Steven Sookhai <stevensookhai@Stevens-MacBook-Pro.local>
2023-09-08 15:32:41 -0400

    heatmap effect?

--- heavenly-beauty-extension/src/App.js
+++ heavenly-beauty-extension/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import WebSocketDataDiv from './WebSocketDataDiv/websocketdatadiv';
+import DotaMapComponent from './DataVisualization/DotaMapComponent';
 
 function App() {
   return (
     <>
       <WebSocketDataDiv />
+      <DotaMapComponent />
     </>
   );
 }
