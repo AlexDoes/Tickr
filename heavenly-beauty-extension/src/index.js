9c9e22f52d238714b040c8f14d6e5964fcc8b3cc
Peter Joh <johpeter@gmail.com>
2023-09-07 22:29:52 -0600

    fixing files

--- heavenly-beauty-extension/src/index.js
+++ heavenly-beauty-extension/src/index.js
@@ -4,13 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <div>hello</div>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+const appRoot = document.getElementById('reactAppRoot');
+
+if (appRoot) {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      appRoot
+    );
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
