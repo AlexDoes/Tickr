ee66eaf132547cd4dab91d6326a3224f563964cf
Peter Joh <johpeter@gmail.com>
2023-09-07 20:54:52 -0600

    Added heavenly-beauty-extension folder

---
+++ heavenly-beauty-extension/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WebSocketDataDiv from './WebSocketDataDiv/websocketdatadiv';
+
+function App() {
+  return (
+    <>
+      <WebSocketDataDiv />
+    </>
+  );
+}
+
+export default App;
