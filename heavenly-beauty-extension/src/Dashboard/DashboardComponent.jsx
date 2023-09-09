bc4ed1aa87fe1130dcd11d27d150f97bb38f1434
AlexDoes <alexsw.191@gmail.com>
2023-09-09 15:54:37 -0400

    Some style and changes to manifest.json

---
+++ heavenly-beauty-extension/src/Dashboard/DashboardComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <button className="btn">Button 1</button>
+      <button className="btn">Button 2</button>
+      <button className="btn">Button 3</button>
+      <button className="btn">Button 4</button>
+    </div>
+  );
+}
+
+export default Dashboard;
