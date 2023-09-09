fa0a76b36a08ec73af68957f26b433af0719df67
Edmund Ju <edmundju@berkeley.edu>
2023-09-09 13:02:10 -0700

    modify script

--- backend/data_files/process-data/get_event_type.py
+++ backend/data_files/process-data/get_event_type.py
@@ -1,10 +1,24 @@
 import json
 
-with open('../dota/DotA-2-The-International-2022/sample-data.json', 'r') as json_file:
-    sample_data = json.load(json_file)
+with open('../dota/DotA-2-The-International-2022/2364966_events.jsonl', 'r') as jsonl_file:
+    for num, line in enumerate(jsonl_file):
+        data = json.loads(line)
+        print(type (data["events    "]))
+        # events = data.get("events", [])
 
-for item in sample_data:
-    events = item.get("events", [])
-    for event in events:
-        event_type = event.get("type", "")
-        print(f'event: {event_type}')
+
+        if num == 2:
+            break
+
+        # events = line.get("events", [])
+        # for event in events:
+        #     event_type = event.get("type", "")
+        #     print(event_type)
+
+# for line in data:
+#     item = json.loads(line)
+#     print(line)
+#     events = item.get("events", [])
+#     for event in events:
+#         event_type = event.get("type", "")
+#         print(f'event: {event_type}')
