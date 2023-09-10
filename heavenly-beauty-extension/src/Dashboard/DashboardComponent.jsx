344292ecbc0e09394225de2d768fd02234f268d7
AlexDoes <alexsw.191@gmail.com>
2023-09-10 04:36:59 -0400

    some clean up on dashboard

--- heavenly-beauty-extension/src/Dashboard/DashboardComponent.jsx
+++ heavenly-beauty-extension/src/Dashboard/DashboardComponent.jsx
@@ -26,33 +26,6 @@ function Dashboard() {
   };
 
   return (
-    // <div className="h-full w-full">
-    //   <div className={`flex flex-col gap-1 bg-purple-400 h-full`}>
-    //     <div className={`h-full w-full`}>
-    //       <h1 className="text-sm w-full text-center">
-    //         DotA The International 2022
-    //       </h1>
-    //       <div className="flex text-green-400 font-semibold rounded-md w-full h-full items-center m-auto justify-evenly z-50">
-    //         {matchMap &&
-    //           Object.keys(matchMap).map((key) => {
-    //             let len = Object.keys(matchMap).length + 1;
-    //             return (
-    //               <button
-    //                 className={`border rounded-md w-1/${len} h-2/3`}
-    //                 key={key}
-    //                 onClick={() => handleButtonClick(key)}
-    //               >
-    //                 {matchMap[key]}
-    //               </button>
-    //             );
-    //           })}
-    //       </div>
-    //     </div>
-    //     <div className="w-full h-full bottom-0">
-    //       {selectedMatch && <WebSocketDataDiv matchId={selectedMatch} />}
-    //     </div>
-    //   </div>
-    // </div>
     <div className="bg-purple-300 h-full">
       {!trackrActive && <Splash function={handleButtonClick} />}
       {trackrActive && <TrackrMain eventId={selectedMatch} />}
