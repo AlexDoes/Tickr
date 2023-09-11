6a896fd7a67d183d131a30f9b6dcc7d304d9247f
Peter Joh <johpeter@gmail.com>
2023-09-11 04:45:51 -0600

    scrolling!

--- heavenly-beauty-extension/src/WebSocketDataDiv/websocketdatadiv.js
+++ heavenly-beauty-extension/src/WebSocketDataDiv/websocketdatadiv.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 function WebSocketDataDiv(props) {
-  const eventId = props.matchId;
-  const websocketData = props.websocketData; 
+  const { matchId: eventId, websocketData } = props;
 
-  console.log(websocketData, '---------------------------------------');
+  const messages = websocketData && typeof websocketData === 'object'
+    ? Object.entries(websocketData)
+    : [];
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      // Scroll the container all the way to the right
+      containerRef.current.scrollLeft = containerRef.current.scrollWidth;
+    }
+  }, [messages]);
 
   return (
-    <div id="websocketDataDiv" className="text-xl font-bold border-yellow-200 border-2">
-      {websocketData && typeof websocketData === 'object' && Object.keys(websocketData).length ? (
-        Object.entries(websocketData).map(([key, value], index) => (
-          <div key={index} className="text-red-700">
+    <div 
+      ref={containerRef}
+      id="websocketDataDiv"
+      className="text-xl font-bold border-yellow-200 border-2 overflow-x-auto whitespace-nowrap"
+    >
+      {messages.length > 0 ? (
+        messages.map(([key, value], index) => (
+          <div key={index} className="text-red-700 inline-block mr-4">
             {key}: {JSON.stringify(value)}
           </div>
         ))
@@ -18,7 +32,7 @@ function WebSocketDataDiv(props) {
         `WebSocket Data Will Appear Here ${eventId}`
       )}
     </div>
-  );  
+  );
 }
 
 export default WebSocketDataDiv;
