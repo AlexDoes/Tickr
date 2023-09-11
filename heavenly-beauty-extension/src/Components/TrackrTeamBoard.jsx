8a7031990349193abec45f3c4c299cfc91408f68
Justin <mancussion@gmail.com>
2023-09-11 01:39:29 -0400

    connected more data to board

--- heavenly-beauty-extension/src/Components/TrackrTeamBoard.jsx
+++ heavenly-beauty-extension/src/Components/TrackrTeamBoard.jsx
@@ -3,8 +3,9 @@ import { stateNormalizer } from "../Resources/normalization";
 import TrackrTeamBoardCard from "./TrackrTeamBoardCard";
 
 export default function TrackrTeamBoard(props) {
-  const [player1, player2, player3, player4, player5] = props.teams[0]
-  const [player6, player7, player8, player9, player10] = props.teams[1]
+  const [team1, team2] = props.teams
+  const [player1, player2, player3, player4, player5] = team1
+  const [player6, player7, player8, player9, player10] = team2
 
   return (
     <div className="h-full w-full flex flex-row border-red-700 justify-evenly xs:text-sm lg:text-lg xl:text-xl relative">
@@ -18,22 +19,22 @@ export default function TrackrTeamBoard(props) {
       </div>
       <div className="h-full border-yellow-200 w-[25%] flex justify-evenly items-center gap-1">
         <div className="flex flex-col gap-1 text-center h-full outline-red-900 w-full justify-between">
-          <p className="teamname1 text-[#37ff44] py-2 text-[2rem] "> CLG </p>
+          <p className="teamname1 text-[#37ff44] py-2 text-[2rem] "> {team1.name} </p>
           <p className="text-[3rem] items-center h-full py-2 flex justify-center text-[#98ff88]">
-            42
+            {team1.kills}
           </p>
-          <p className="text-[1.5rem] mb-2 text-slate-400 py-2">42/49/72</p>
+          <p className="text-[1.5rem] mb-2 text-slate-400 py-2">{`${team1.kills}/${team1.deaths}/${team1.assists}`}</p>
         </div>
         <img
           className="h-full w-[h-full]"
           src="https://private-user-images.githubusercontent.com/91306408/266902749-81d2faa9-f0cb-4a5d-b095-fd3b6fda51ae.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2OTQ0MDgyMDUsIm5iZiI6MTY5NDQwNzkwNSwicGF0aCI6Ii85MTMwNjQwOC8yNjY5MDI3NDktODFkMmZhYTktZjBjYi00YTVkLWIwOTUtZmQzYjZmZGE1MWFlLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzA5MTElMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMwOTExVDA0NTE0NVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTA3MDBjZGIwNTMxNTJjNGQzMjkwMWY1ZWM5ZThiYzcwNTM0OGJiNDc0Nzc1ZWViMDM4ZDRlMjNhZDQyOGM1N2YmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.TMBvK3qlH5ncH7YTh6-Tqzngdd4sL030MzcAyqEOphE"
         ></img>
         <div className="flex flex-col gap-1 text-center h-full outline-red-900 w-full justify-between">
-          <p className="teamname2 text-[#ff6666] py-2 text-[2rem]"> TSM </p>
+          <p className="teamname2 text-[#ff6666] py-2 text-[2rem]"> {team2.name} </p>
           <p className="text-[3rem] items-center h-full py-2 flex justify-center text-[#ba3232]">
-            49
+            {team2.kills}
           </p>
-          <p className="text-[1.5rem] mb-2 text-slate-400 py-2">49/42/78</p>
+          <p className="text-[1.5rem] mb-2 text-slate-400 py-2">{`${team2.kills}/${team2.deaths}/${team2.assists}`}</p>
         </div>
       </div>
       <div className="flex flex-col  xs:text-[.5rem] lg:text-[1rem] xl:text-[1rem] w-2/8 h-full justify-evenly">
