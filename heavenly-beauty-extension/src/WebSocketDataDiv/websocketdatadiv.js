190cec723705bb7333abb0ab86e4008c4b05a819
Peter Joh <johpeter@gmail.com>
2023-09-11 12:43:58 -0600

    scrolling ticker almost

--- heavenly-beauty-extension/src/WebSocketDataDiv/websocketdatadiv.js
+++ heavenly-beauty-extension/src/WebSocketDataDiv/websocketdatadiv.js
@@ -2,39 +2,54 @@ import React, { useState, useEffect } from "react";
 
 function WebSocketDataDiv(props) {
   const { websocketData } = props;
-  const [lastValidData, setLastValidData] = useState(null);
-
-  // Check if the data has an eventType
-  const hasEventType = websocketData['message'] !== "Default message for unhandled event type";
+  const [dataQueue, setDataQueue] = useState([]);
 
   useEffect(() => {
-    // If the data has an eventType, update the lastValidData state
-    if (hasEventType) {
-      setLastValidData(websocketData);
+    if (websocketData.message !== "Default message for unhandled event type") {
+      // Add new data to the front of the queue
+      setDataQueue((prevQueue) => [websocketData, ...prevQueue]);
     }
-  }, [hasEventType, websocketData]);
+  }, [websocketData]);
+
+  const containerStyle = {
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    width: "100%",
+    animation: "scroll 20s linear infinite", // Adjust the animation duration as needed
+  };
+
+  const contentStyle = {
+    display: "inline-block",
+  };
+
+  const itemStyle = {
+    marginRight: "10px", // Adjust spacing between items
+  };
 
   return (
-    <div className="text-xl font-bold border-yellow-200 border-2 overflow-x-auto whitespace-nowrap">
-      {hasEventType ? (
-        <div className="text-red-700 inline-block mr-4">
-          {Object.keys(websocketData).map((key) => (
-            <div key={key}>
-              {key}: {websocketData[key]}
-            </div>
-          ))}
-        </div>
-      ) : lastValidData ? (
-        <div className="text-red-700 inline-block mr-4">
-          {Object.keys(lastValidData).map((key) => (
-            <div key={key}>
-              {key}: {lastValidData[key]}
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div>WebSocket Data Will Appear Here</div>
-      )}
+    <div className="text-xl font-bold border-yellow-200 border-2 overflow-hidden">
+      <div className="text-red-700 inline-block" style={containerStyle}>
+        {dataQueue.length > 0 ? (
+          <div style={contentStyle}>
+            {dataQueue.map((data, index) => (
+              <div
+                key={index}
+                style={itemStyle}
+                className="scroll-item"
+              >
+                {Object.keys(data).map((key) => (
+                  <div key={key}>
+                    {key}: {data[key]}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>WebSocket Data Will Appear Here</div>
+        )}
+      </div>
     </div>
   );
 }
